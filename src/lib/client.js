36f5b4c7079ce722d6a6de60608ff238caaea969
Nikolai Onken <nikolai@nikolaionken.com>
2020-07-14 15:08:30 +0200

    Precise imports.

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,19 +1,19 @@
-import AWS from 'aws-sdk';
+import { config, DynamoDB } from 'aws-sdk';
 import { getTransactWrite } from './transact-write';
 
 const tableName = process.env.BASELINE_FIREPLACE_DYNAMODB_TABLENAME;
 
-AWS.config.update({ region: process.env.BASELINE_FIREPLACE_DYNAMODB_REGION });
+config.update({ region: process.env.BASELINE_FIREPLACE_DYNAMODB_REGION });
 
 if (process.env.BASELINE_FIREPLACE_DYNAMODB_ENDPOINT) {
-  AWS.config.update({
+  config.update({
     dynamodb: {
       endpoint: process.env.BASELINE_FIREPLACE_DYNAMODB_ENDPOINT
     }
   });
 }
 
-const docClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+const docClient = new DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 const transactWrite = getTransactWrite(docClient);
 
 export {
